a7fa335ac123dd6a10df283a4bd6358c3e381ac4
guguiness <gus.nogueira@protonmail.com>
2022-07-31 00:11:34 -0300

    fix: Constructor removed

--- Note.cs
+++ Note.cs
@@ -9,14 +9,14 @@ namespace NoteApp {
         string title;
         string content;
 
-        public Note(string title, string content) {
-            this.title = title;
-            this.content = content;
-        }
-
         public string Title {
             get { return this.title; }
             set { this.title = value; }
+        }
+
+        public string Content {
+            get { return this.content; }
+            set { this.content = value; }
         }
     }
 }
